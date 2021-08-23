00002041c9ac343a9e07aecb042345c6d60fcc71
rachaelkim831 <63431636+rachaelkim831@users.noreply.github.com>
2021-08-23 10:31:20 -0700

    completed js primitives and variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,17 @@
+const fullName = 'Rachael Kim';
+console.log('value of fullName:', fullName);
+const isCool = true;
+console.log('value of isCool:', isCool);
+const totalPets = 2;
+console.log('value of totalPets:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets', typeof totalPets);
+
+const unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typof unknown:', typeof unknown);
+let never;
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
