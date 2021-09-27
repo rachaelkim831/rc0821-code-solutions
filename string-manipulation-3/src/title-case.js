b8b2fe56f9ba2cea44560f82636bcc0cc1c394da
rachaelkim831 <63431636+rachaelkim831@users.noreply.github.com>
2021-09-26 18:30:04 -0700

    completed sm-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,14 +1,52 @@
 /* exported titleCase */
+
 function titleCase(title) {
-  title = title.toLowerCase().split(' ');
-  for (let i = 0; i < title.length; i++) {
-    title[i] = title[i].charAt(0).toUpperCase() + title[i].slice(1);
-    if (title[i].toLowerCase().includes('javascript')) {
-      title[i] = 'JavaScript';
-    }
-    if (title[i].toLowerCase().includes('api')) {
-      title[i] = 'API';
+
+  var specialCheck = {
+    javascript: 'JavaScript',
+    api: 'API',
+    web: 'Web'
+  };
+  var splitArr = title.split(' ');
+  if (splitArr[0].includes(':')) {
+    splitArr[0] = splitArr[0].substr(0, splitArr[0].length - 1);
+    if (splitArr[0].toLowerCase() in specialCheck) {
+      splitArr[0] = specialCheck[splitArr[0].toLowerCase()] + ':';
+    } else {
+      splitArr[0] = splitArr[0][0].toUpperCase() + splitArr[0].slice(1) + ':';
     }
   }
-  return title.join(' ');
+  if (splitArr[0].toLowerCase() in specialCheck) {
+    splitArr[0] = specialCheck[splitArr[0].toLowerCase()];
+  }
+
+  splitArr[0] = splitArr[0][0].toUpperCase() + splitArr[0].slice(1);
+  for (let i = 1; i < splitArr.length; i++) {
+    if (splitArr[i].length > 3) {
+      splitArr[i] = splitArr[i][0].toUpperCase() + splitArr[i].slice(1);
+    }
+    if (splitArr[i].includes(':')) {
+      splitArr[i] = splitArr[i].substr(0, splitArr[i].length - 1);
+      if (splitArr[i].toLowerCase() in specialCheck) {
+        splitArr[i] = specialCheck[splitArr[i].toLowerCase()] + ':';
+      } else {
+        splitArr[i] = splitArr[i][0].toUpperCase() + splitArr[i].slice(1) + ':';
+      }
+    }
+    if (splitArr[i].toLowerCase() in specialCheck) {
+      splitArr[i] = specialCheck[splitArr[i].toLowerCase()];
+    }
+  }
+  var tempStr = splitArr.join(' ');
+  if (tempStr.includes(':')) {
+    var tempStrArr = tempStr.split(': ');
+    tempStrArr[1] = tempStrArr[1][0].toUpperCase() + tempStrArr[1].slice(1);
+    tempStr = tempStrArr.join(': ');
+  }
+  if (tempStr.includes('-')) {
+    var tempStrArr2 = tempStr.split('-');
+    tempStrArr2[1] = tempStrArr2[1][0].toUpperCase() + tempStrArr2[1].slice(1);
+    tempStr = tempStrArr2.join('-');
+  }
+  return tempStr;
 }
