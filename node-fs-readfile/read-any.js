f9d12cc3257770e5bbf7143e89265c0f77f276e9
rachael kim <rachaelkim831@gmail.com>
2021-10-06 15:18:49 -0700

    revision

--- node-fs-readfile/read-any.js
+++ node-fs-readfile/read-any.js
@@ -1,24 +1,8 @@
 const fs = require('fs');
 
-if (process.argv[2] === 'hopper.txt') {
-  fs.readFile('./hopper.txt', 'utf8', (err, data) => {
-    if (err) {
-      throw err;
-    }
-    console.log(data);
-  });
-} else if (process.argv[2] === 'dijkstra.txt') {
-  fs.readFile('./dijkstra.txt', 'utf8', (err, data) => {
-    if (err) {
-      throw err;
-    }
-    console.log(data);
-  });
-} else if (process.argv[2] === 'cunningham.txt') {
-  fs.readFile('./cunningham.txt', 'utf8', (err, data) => {
-    if (err) {
-      throw err;
-    }
-    console.log(data);
-  });
-}
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log(data);
+});
