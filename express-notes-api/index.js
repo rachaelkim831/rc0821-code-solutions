1f38e3ecb3aff36833aaf0439e84437038c8324d
rachael kim <rachaelkim831@gmail.com>
2021-10-11 17:26:40 -0700

    revised express-notes-api

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -20,29 +20,26 @@ app.get('/api/notes/:id', (req, res) => {
   if (isNaN(req.params.id) || req.params.id < 0) {
     res.status(400).send({ error: 'id must be a positive integer' });
   }
-  if (req.params.id !== notes.id) {
+  if (notes[req.params.id]) {
+    res.status(200).send(notes[req.params.id]);
+  } else {
     return res.status(404).send({ error: `cannot find note with id ${req.params.id}` });
-  }
-  for (const property in notes) {
-    if (property === req.params.id) {
-      res.status(200).send(notes[property]);
-    }
   }
 });
 
 // POST
 
 app.post('/api/notes', (req, res) => {
-  if (req.body.content === undefined) {
+  if (!req.body.content) {
     res.status(400).send({ error: 'content is a required field' });
   }
   const newNote = { id: data.nextId, ...req.body };
   data.notes = { ...data.notes, [data.nextId]: newNote };
-  data.nextId++;
-  if (!data.notes[newNote.id]) {
-    return res.status(500).send({ error: 'An unexpected error occurred.' });
+  if (data.notes[newNote.id]) {
+    data.nextId++;
+    return res.status(201).send({ content: `${req.body.content}` });
   }
-  res.status(201).send({ content: 'Inertia is a property of matter.' });
+  return res.status(500).send({ error: 'An unexpected error occurred.' });
 });
 
 // DELETE
@@ -51,18 +48,16 @@ app.delete('/api/notes/:id', (req, res) => {
   if (isNaN(req.params.id) || req.params.id < 0) {
     return res.status(400).send({ error: 'id must be a positive integer' });
   }
-  if (req.params.id === undefined) {
+  if (!data.notes[req.params.id]) {
     return res.status(404).send({ error: `cannot find note with id ${req.params.id}` });
   }
   const notes = data.notes;
-  for (const property in notes) {
-    if (property === req.params.id) {
-      delete notes[property];
-      return res.sendStatus(204);
-    }
-  }
   if (notes[req.params.id]) {
-    return res.status(500).send({ error: 'An unexpected error occurred.' });
+    delete notes[req.params.id];
+    if (notes[req.params.id]) {
+      return res.status(500).send({ error: 'An unexpected error occurred.' });
+    }
+    return res.sendStatus(204);
   }
 });
 
@@ -79,11 +74,9 @@ app.put('/api/notes/:id', (req, res) => {
     return res.status(404).send({ error: `cannot find note with id ${req.params.id}` });
   }
   const notes = data.notes;
-  for (const property in notes) {
-    if (req.params.id === property) {
-      notes[property].content = req.body.content;
-      return res.status(200).send({ content: `The event loop, this, closures, and prototypal inheritance are special about JavaScript. ${req.params.id}` });
-    }
+  if (req.params.id) {
+    notes[req.params.id].content = req.body.content;
+    return res.status(200).send({ content: 'The event loop, this, closures, and prototypal inheritance are special about JavaScript.', id: `${req.params.id}` });
   }
   if (notes[req.params.id].content !== req.body.content) {
     return res.status(500).send({ error: 'An unexpected error occurred.' });
