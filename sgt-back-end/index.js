88bed4cf9be309bdff195af2185e9212e456fe55
rachael kim <rachaelkim831@gmail.com>
2021-10-14 17:21:26 -0700

    revised

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -19,7 +19,7 @@ app.get('/api/grades', (req, res) => {
   db.query(sql)
     .then(data => {
       const grades = data.rows;
-      res.status(201).json(grades);
+      res.status(200).json(grades);
     })
     .catch(err => {
       console.log('Get grades error:', err);
@@ -91,24 +91,17 @@ app.put('/api/grades/:gradeId', (req, res) => {
       "course" = $2,
       "score" = $3
   where "gradeId" = $4
+  returning *
   `;
   const params = [name, course, score, gradeId];
 
   db.query(sql, params)
     .then(data => {
+      const [grade] = data.rows;
       if (data.rowCount === 0) {
         return res.status(404).json({ error: `Cannot find grade with "gradeId" ${gradeId}` });
       }
-      if (data.rowCount === 1) {
-        const params2 = [gradeId];
-        const sql2 = `select * from grades
-        where "gradeId" = $1`;
-        db.query(sql2, params2)
-          .then(data => {
-            const grades = data.rows[0];
-            res.status(200).json(grades);
-          });
-      }
+      res.status(200).json(grade);
     })
     .catch(err => {
       console.log('Update grade error:', err);
@@ -118,7 +111,6 @@ app.put('/api/grades/:gradeId', (req, res) => {
 
 app.delete('/api/grades/:gradeId', (req, res) => {
   const gradeId = req.params.gradeId;
-
   if (gradeId <= 0 || isNaN(gradeId)) {
     return res.status(400).json({ error: '"gradeId" must be a positive integer' });
   }
