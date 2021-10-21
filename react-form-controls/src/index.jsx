f17b4abddf2fe451fd9f1cf5229304f9bd15a856
rachael kim <rachaelkim831@gmail.com>
2021-10-21 13:33:32 -0700

    completed react form controls

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('State:', this.state);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Email:
+          <input type="email" value={this.state.value} onChange={this.handleChange} name="email"></input>
+        </label>
+        <button>Sign Up</button>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NewsletterForm />,
+  document.getElementById('root')
+);
