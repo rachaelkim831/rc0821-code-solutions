9b51fda41fe9e958284454796207cd7764680c5a
rachael kim <rachaelkim831@gmail.com>
2021-10-25 12:49:55 -0700

    completed fetch

---
+++ fetch/main.js
@@ -0,0 +1,18 @@
+
+fetch('https://jsonplaceholder.typicode.com/users', {
+  method: 'GET'
+})
+  .then(response => response.json())
+  .then(data => console.log([data]))
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+fetch('https://pokeapi.co/api/v2/pokemon/1', {
+  method: 'GET'
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => {
+    console.error('Error:', error);
+  });
